31b030fddea1348719abddb8a7ff610058aa25e8
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2021-09-13 19:23:49 +0100

    Compose ConstraintLayout

--- app/src/main/java/com/electrosalaf/composelayout/DecoupledConstraint.kt
+++ app/src/main/java/com/electrosalaf/composelayout/DecoupledConstraint.kt
@@ -6,27 +6,51 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
+import com.electrosalaf.composelayout.ui.theme.ComposeLayoutTheme
+
+@Composable
+fun ConstraintLayoutContentSample() {
+  ConstraintLayout() {
+
+    val (button, text) = createRefs()
+
+    Button(
+      onClick = { /*TODO*/ }, 
+      modifier = Modifier.constrainAs(button) {
+        top.linkTo(parent.top, margin = 16.dp)
+      }
+    ) {
+      Text("Button")
+    }
+    
+    Text("Text", modifier = Modifier.constrainAs(text) {
+      top.linkTo(button.bottom, margin = 16.dp)
+    })
+  }
+}
 
 @Composable
 fun DecoupledConstraintLayout() {
   BoxWithConstraints() {
     val constraints = if (maxWidth < minHeight) {
-      decoupledConstraints(margin = 16.dp)
+      decoupledConstraints(margin = 16.dp)  // portrait constraints
     } else {
-      decoupledConstraints(margin = 32.dp)
+      decoupledConstraints(margin = 32.dp)  // Landscape constraints
     }
     
-    ConstraintLayout() {
+    ConstraintLayout(constraints) {
       Button(
         onClick = { /*TODO*/ },
         modifier = Modifier.layoutId("button")
       ) {
         Text("Button")
       }
+
       Text("Text", Modifier.layoutId("text"))
     }
   }
@@ -43,5 +67,21 @@ private fun decoupledConstraints(margin: Dp): ConstraintSet {
     constrain(text) {
       top.linkTo(button.bottom, margin)
     }
+  }
+}
+
+@Preview
+@Composable
+fun ConstraintLayoutContentSamplePreview() {
+  ComposeLayoutTheme() {
+    ConstraintLayoutContentSample()
+  }
+}
+
+@Preview
+@Composable
+fun DecoupledConstraintPreview() {
+  ComposeLayoutTheme {
+    DecoupledConstraintLayout()
   }
 }
